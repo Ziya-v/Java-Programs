fdeb54752308c5db59625d48201bb05b0df0e451
Ziya-v <shubhangi005@gmail.com>
2026-06-02 18:30:05 +0530

    Updated Linearsearch and compiled SB

--- Linearsearch.java
+++ Linearsearch.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class Linearsearch{
-    public static int seq_search(int arr[],int e){
+    public int seq_search(int arr[],int e){
         int r=0;
         for(int i=1;i<10;i++)
         {
@@ -13,23 +13,24 @@ class Linearsearch{
 return r;
 }
     public static void main(String args[]){
-        Scanner in=new Scanner(System.in);
-        int[] a= new int[10];
-        int val;int x;
-        System.out.println("Enter an array");
-        for(int i=0;i<10;i++){
-
-        a[i]=in.nextInt();
-
-    }
-    System.out.println("Enter value to search");
-    val=in.nextInt();
-
-    Linearsearch ob=new Linearsearch();
-    x=ob.seq_search(a,val);
-    if(x==1)
-    System.out.println("Item found");
-    else
-     System.out.println("Item not found");
+        try (Scanner in = new Scanner(System.in)) {
+            int[] a= new int[10];
+            int val;int x;
+            System.out.println("Enter an array");
+            for(int i=0;i<10;i++){
+                
+                a[i]=in.nextInt();
+                
+            }
+            System.out.println("Enter value to search");
+            val=in.nextInt();
+            
+            Linearsearch ob=new Linearsearch();
+            x=ob.seq_search(a,val);
+            if(x==1)
+                System.out.println("Item found");
+            else
+                System.out.println("Item not found");
+        }
     }}
     
